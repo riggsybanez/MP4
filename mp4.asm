966797d43811fba55f822f8bfa5a317d3531c297
riggsybanez <142960686+riggsybanez@users.noreply.github.com>
2024-03-26 10:09:28 +0800

    Update mp4.asm

--- mp4.asm
+++ mp4.asm
@@ -1,1 +1,35 @@
+section .bss
+    buffer resb 1
+
+section .text
+    global _start
+
+_start:
+    
+    mov eax, 3           
+    mov ebx, 0           
+    mov ecx, buffer      
+    mov edx, 1          
+    int 80h             
+ 
+    cmp byte [buffer], 'A'
+    jl character_not_lowercase
+    cmp byte [buffer], 'Z'
+    jg character_not_lowercase
+
+
+    add byte [buffer], 32  
+
+character_not_lowercase:
+
+    mov eax, 4           
+    mov ebx, 1          
+    mov ecx, buffer      
+    mov edx, 1           
+    int 80h            
+
+
+    mov eax, 1           
+
+
 
